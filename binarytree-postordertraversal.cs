44e1838fea9f3b94cb9e9bf3cfc21df31a5f8fb3
John Miller <1631300+jwmiller5@users.noreply.github.com>
2020-04-18 12:05:45 -0400

    37% runtime 0% memory

---
+++ binarytree-postordertraversal.cs
@@ -0,0 +1,28 @@
+
+ using System.Collections.Generic;
+ namespace leetcode
+ {
+     public partial class Solution {
+         /// left, then right, then root
+        public IList<int> PostorderTraversal(TreeNode root) {
+            List<int> inorder = new List<int>();            
+            return ProcessNodePostOrder(root, inorder);
+        }
+
+        public List<int> ProcessNodePostOrder(TreeNode current, List<int> prev)
+        {
+            if (current == null)
+                return prev;
+
+            if (current.left != null)
+                ProcessNodePostOrder(current.left, prev);
+
+            if (current.right != null)
+                ProcessNodePostOrder(current.right, prev);
+                        
+            prev.Add(current.val);
+
+            return prev;
+        }
+    }
+ }
